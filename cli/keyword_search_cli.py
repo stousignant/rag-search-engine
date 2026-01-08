3204ae78943a98a86cef34d2058950ec3e3bf621
stousignant <sacha.tousignant@gmail.com>
2026-01-08 15:58:38 -0500

    Initial commit + Implement keyword search CLI on movie data set

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+from lib.keyword_search import search_command
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            results = search_command(args.query)
+            for i, result in enumerate(results, start=1):
+                print(f"{i}. {result['title']}")
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
